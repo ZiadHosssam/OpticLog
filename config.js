677583afba59681349bb32d4212a996667415c3c
ziadhosssam <ziad.h.ramadona@gmail.com>
2026-05-13 22:54:42 +0300

    config update

--- config.js
+++ config.js
@@ -1,4 +1,10 @@
+let localSecrets = {};
+try {
+    localSecrets = await import('./secrets.js');
+} catch (e) {
+}
+
 export const config = {
-    supabaseUrl: url,
-    supabaseKey: key
+    supabaseUrl: localSecrets.SUPABASE_URL || "", 
+    supabaseKey: localSecrets.SUPABASE_ANON_KEY || ""
 };

5deb32c90ee3b1253533d5634c7cc8ae99a9c166
ziadhosssam <ziad.h.ramadona@gmail.com>
2026-05-13 11:58:51 +0300

    login and signup buttons working

--- app.js
+++ app.js
@@ -1,6 +1,60 @@
-import CONFIG from './config.js';
+const loginModal = document.getElementById('login-modal');
+const signupModal = document.getElementById('signup-modal');
+const profileMenu = document.getElementById('profile-menu');
 
-const SUPABASE_URL = CONFIG.SUPABASE_URL;
-const SUPABASE_ANON_KEY = CONFIG.SUPABASE_ANON_KEY;
+// Controling Popus
 
-const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.openLoginModal = function() {
+    loginModal.style.display = 'flex';
+    signupModal.style.display = 'none';
+};
+window.closeLoginModal = function() {
+    loginModal.style.display = 'none';
+};
+window.openSignupModal = function() {
+    signupModal.style.display = 'flex';
+    loginModal.style.display = 'none';
+};
+window.closeSignupModal = function() {
+    signupModal.style.display = 'none';
+};
+
+window.onclick = function(event) {
+    if (event.target === loginModal) {
+        closeLoginModal();
+    }
+    if (event.target === signupModal) {
+        closeSignupModal();
+    }
+};
+
+// Popup Links
+
+window.swtichToSignup = function() {
+    closeLoginModal();
+    openSignupModal();
+};
+
+window.switchToLogin = function() {
+    closeSignupModal();
+    openLoginModal();
+};
+
+// Profile
+window.toggleProfile = function() {
+    if (profileMenu.style.display === 'none' || profileMenu.style.display === '') {
+        profileMenu.style.display = 'block';
+    }
+    else {
+        profileMenu.style.display = 'none';
+    }
+};
+
+
+window.toggleProfile = function() {
+    if (profileMenu.style.display === 'none' || profileMenu.style.display === '') {
+        profileMenu.style.display = 'block';
+    } else {
+        profileMenu.style.display = 'none';
+    }
+};

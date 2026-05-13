93b2d7ee3cc87c8533b3c5f8e8ebcab24b855d9b
ziadhosssam <ziad.h.ramadona@gmail.com>
2026-05-13 12:31:52 +0300

    login and signup and logout logic working

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const loginModal = document.getElementById('login-modal');
 const signupModal = document.getElementById('signup-modal');
 const profileMenu = document.getElementById('profile-menu');
 
+
+
 // Controling Popus
 
 window.openLoginModal = function() {
@@ -58,3 +60,94 @@ window.toggleProfile = function() {
         profileMenu.style.display = 'none';
     }
 };
+
+// Login & Logout & Signup
+
+window.signUp = async function() {
+    const email = document.getElementById('signup-email').value;
+    const password = document.getElementById('signup-password').value;
+    const confirmPassword = document.getElementById('signup-confirm-password').value;
+
+    if (password !== confirmPassword) {
+        alert('Passwords do not match!');
+        return;
+    }
+
+    const {data, error} = await supabase.auth.signUp({email, password});
+
+    if (error) {
+        alert(error.message);
+    }
+    else {
+        alert("Check Your Email For Verification!!");
+        closeSignupModal();
+    }
+}
+
+
+window.login = async function () {
+    const email = document.getElementById('signup-email').value;
+    const password = document.getElementById('signup-password').value;
+
+    const {data, error} = await supabase.auth.signInWithPassword({ email, password});
+
+    if (error) {
+        alert(error.message);
+    }
+    else{
+        closeLoginModal();
+        checkUser();
+    }
+}
+
+window.logout = async function() {
+    await supabase.auth.signOut;
+    location.reload();
+}
+
+// Controling Landing Page
+
+async function checkUser() {
+    const {data: {user}} = await supabase.auth.getUser();
+
+    if (user) {
+        heroSection.style.display = 'none';
+        dashboardArea.style.display = 'none';
+        profileBtn.style.display = 'flex';
+
+        loadPrescriptionHistory(user.id);
+        loadProfileData(user.id);
+    }
+    else {
+        heroSection.style.display = 'flex';
+        dashboardArea.style.display = 'none';
+        profileBtn.style.display = 'none';
+    }
+}
+
+checkUser();
+
+// Fetching Data
+
+async function loadPrescriptionHistory(userId) {
+    const {data, error} = await supabase.from('prescriptions').select('*').eq('user_id', userId).order('created_at', {ascending: false});
+    if (error) {
+        console.error('Error fetching prescription history:', error);
+    }
+
+    const tbody = document.getElementById('history-body');
+    tbody.innerHTML = '';
+
+    data.forEach(row => {
+        tbody.innerHTML += `
+            <tr>
+                <td>${new Date(row.created_at).toLocaleDateString()}</td>
+                <td>${row.eye}</td>
+                <td>${row.sph}</td>
+                <td>${row.cyl}</td>
+                <td>${row.axis}</td>
+            </tr>
+        `
+    });
+}
+
